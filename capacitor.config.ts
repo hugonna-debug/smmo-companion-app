28c98f7b6c009b05ca7bcb456932e126c3f2654b
hugonna-debug <hudsonjohnalan@gmail.com>
2026-05-18 21:37:13 +0800

    feat: initialize capacitor for android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.smmo.companion',
+  appName: 'SMMO Companion',
+  webDir: 'dist'
+};
+
+export default config;
